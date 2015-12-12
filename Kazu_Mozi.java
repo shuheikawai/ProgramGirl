5501a8684332f79d3db2ed4c9190084671c14935
shuheikawai <for.my.university@gmail.com>
2015-12-12 17:47:26 +0900

    眼鏡獲得用

---
+++ Kazu_Mozi.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Kazu_Mozi {
+    public static void main(String[] args) throws Exception {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int i;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int j = Integer.parseInt(line);
+        for(i=0;i<j;i++){
+        System.out.print("Ann");
+      }
+    }
+}
